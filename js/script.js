77b5aecd06c96b4993bd941f931cf61f56e66706
Le6ya <liliatolstyh@gmail.comgit config --global user.email liliatolstyh@gmail.com>
2023-03-16 13:52:54 +0300

    slider

---
+++ js/script.js
@@ -0,0 +1,14 @@
+new Swiper('.hero__slider', {
+  slidesPerview: 2,
+  spaceBetween: 10,
+  loop: true,
+
+navigation: {
+  prevEl: '.hero__slider-btn_prev',
+  nextEl: '.hero__slider-btn_next',
+},
+
+// autoplay: {
+//   delay: 3000,
+// }
+})
